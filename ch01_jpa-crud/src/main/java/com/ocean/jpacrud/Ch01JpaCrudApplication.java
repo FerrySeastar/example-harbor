30abce0f759be4d4f9a0d94829321ccc070b1792
FerryLa <rlawl4240@gmail.com>
2025-06-15 22:11:15 +0900

    feat : shop

---
+++ ch01_jpa-crud/src/main/java/com/ocean/jpacrud/Ch01JpaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.jpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch01JpaCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch01JpaCrudApplication.class, args);
+    }
+
+}
